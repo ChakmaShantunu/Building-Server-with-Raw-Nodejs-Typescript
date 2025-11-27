84c308b31cbe0e5d04146490f576e379eed45dc7
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-27 23:10:06 +0600

    implement modular dynamic routing system with helpers

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,7 @@ import http, { IncomingMessage, Server, ServerResponse } from 'http';
 import config from './config';
 import addRoutes, { RouteHandler, routes } from './helpers/RouteHandler';
 import path from 'path';
-
-addRoutes("GET", "/", (req, res) => {
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify(
-        {
-            message: "Hello from node js with typescript...",
-            path: req.url,
-        }
-    ))
-})
-
+import "./routes";
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
     console.log("Server is running...");
@@ -34,25 +24,6 @@ const server: Server = http.createServer((req: IncomingMessage, res: ServerRespo
         }))
     }
 
-    // if (req.url == "/" && req.method == "GET") {
-    //     res.writeHead(200, { "content-type": "application/json" });
-    //     res.end(JSON.stringify(
-    //         {
-    //             message: "Hello from node js with typescript...",
-    //             path: req.url,
-    //         }
-    //     ))
-    // }
-
-    // if (req.url == '/api' && req.method == "GET") {
-    //     res.writeHead(200, { "content-type": "application/json" });
-    //     res.end(JSON.stringify(
-    //         {
-    //             message: "Health status ok",
-    //             path: req.url,
-    //         }
-    //     ))
-    // }
 
 
     // if (req.url == '/api/users' && req.method == "POST") {
